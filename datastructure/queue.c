041e9b448ccf7a60afb61acba77944fd1cfec721
xingxing.wang <xingxing-wang@qq.com>
2014-12-14 23:08:46 +0800

    添加queue的实现

--- datastructure/queue.c
+++ datastructure/queue.c
@@ -4,7 +4,7 @@
 
 Queue* queue_create()
 {
-	Queue* q = (Queue*)malloc(sizeof(Queue));	
+	Queue* q = (Queue*)malloc(sizeof(Queue));
 	if (!q) {
 		printf("memory error!\n");
 		return NULL;
@@ -17,5 +17,31 @@ Queue* queue_create()
 
 Queue* queue_add(Queue *q, int value)
 {
-	return NULL;
+	Queue* p = (Queue*)malloc(sizeof(Queue));
+	if (!p) {
+		printf("memory error!\n");
+		return NULL;
+	}
+	p = first;
+	p->value = value;
+	q->size = size++;
+	return q;
 }
+
+Queue* queue_pop(Queue *q)
+{
+    Queue *p
+    p = last;
+    last->next = last;
+    free(p);
+    return q;
+}
+
+
+
+void queue_clear(Queue *q)
+{
+    while (first != NULL)
+        first = queue_pop(q);
+}
+
