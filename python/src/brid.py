158fe435d3ad55fbc6c4ee512f418e9196695797
xingxing.wang <xingxing.wang@tqmall.com>
2015-01-03 22:19:14 +0800

    modify some python file

--- python/src/brid.py
+++ python/src/brid.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-  
 
+
 class Bird(object):
-		
-	have_feather = True
-	way_of_reproduction = 'egg'
-	
-	def move(self, dx, dy):
-		position = [0, 0]
-		position[0] += dx
-		position[1] += dy
-		return position
-	
-	
+    have_feather = True
+    way_of_reproduction = 'egg'
+
+    def move(self, dx, dy):
+        print self.have_feather
+        position = [0, 0]
+        position[0] += dx
+        position[1] += dy
+        return position
+
+
 class Chicken(Bird):
-	
-	way_of_move = 'walk'
-	possible_in_KFC = 'True'
+    way_of_move = 'walk'
+    possible_in_KFC = 'True'
