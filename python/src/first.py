1ba54e1b40727dc7c668b390677b155f00809511
yanlin.shao <yanlin.shao@outlook.com>
2015-07-04 19:40:27 +0800

    yl add python file

---
+++ python/src/first.py
@@ -0,0 +1,6 @@
+def laugh():
+	print 'hahahaha'
+	
+
+
+
--- python/src/main.py
+++ python/src/main.py
@@ -1,40 +1,26 @@
 # -*- coding: utf-8 -*-  
-from brid import Chicken, Bird
+    
+class bird(object):
+    feather = True
+    
+class chicken(bird):
+    fly = False
+    def __init__(self, age):
+        self.age = age
+    def _getattr_(self, name):
+        if name == 'adult':
+            if self.age > 1.0:
+                return True
+            else :
+                return False
+        else :
+            raise AttributeError(name)
+summer = chicken(2)
 
-def square_sum(a, b):
-    c = a ** 2 + b ** 2
-    return a, b, c
+print (summer.adult)
+summer.age = 0.5
+print (summer.adult)
+
+print (summer.male) 
 
 
-def change_int(x):
-    x += 1
-    return x
-
-def change_list(data):
-    data[1] = 3
-    return data
-
-
-if __name__ == '__main__':
-    summer = Chicken()
-    print summer.have_feather
-    print summer.move(5, 8)
-    summer = Bird()
-    print 'after move:', summer.move(5, 8)
-    idx = range(5)
-    print idx
-    for a in range(10):
-        print a ** 2
-    i = 1
-    while i < 10:
-        print i
-        i += 1
-    for a in range(7):
-        if a == 3:
-            continue
-        print a
-    for a in range(6):
-        if a == 4:
-            break
-        print a
-    print square_sum(4, 3)
