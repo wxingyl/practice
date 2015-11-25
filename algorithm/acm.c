7f27b1b0811dc8a04af8aceb77f27f0f14227c81
xing <xing@xingdeMacBook-Pro.local>
2015-11-26 00:08:27 +0800

    add maxProfit issue

--- algorithm/acm.c
+++ algorithm/acm.c
@@ -493,5 +493,41 @@ int maximalSquare(char** matrix, int matrixRowSize, int matrixColSize) {
  * see python implement
  */
 char** restoreIpAddresses(char* s, int* returnSize) {
-	return NULL;	    
+	return NULL;
 }
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize < 2) return 0;
+    int* p = (int*) malloc(sizeof(int) * pricesSize);
+    p[0] = 0;
+    int sell = -1;
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] <= prices[i-1]) {
+            p[i] = p[i-1];
+            continue;
+        }
+        int diff = prices[i] - prices[i-1];
+        if (sell == i-1) {
+            p[i] = p[i-1] + diff;
+        } else if (sell > 0){
+            p[i] = p[i-3] + diff;
+            if (prices[i] > prices[sell]) {
+                p[i] = max(p[i-1] + prices[i] - prices[sell], p[i]);
+            } else if (sell == i-2 && p[i] < p[i-1]){
+                p[i] = p[i-1];
+                continue;
+            }
+        } else {
+            p[i] = diff;
+        }
+        sell = i;
+    }
+    int ret = p[pricesSize-1];
+    free(p);
+    return ret;
+}
+
--- algorithm/acm.h
+++ algorithm/acm.h
@@ -240,4 +240,26 @@ int maximalSquare(char** matrix, int matrixRowSize, int matrixColSize);
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** restoreIpAddresses(char* s, int* returnSize);
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * Say you have an array for which the ith element is the price of a given stock
+ * on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (ie, buy one and sell one share of the stock
+ * multiple times) with the following restrictions:
+ *
+ * You may not engage in multiple transactions at the same time (ie, you must
+ * sell the stock before you buy again).
+ * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1
+ * day)
+ * Example:
+ *
+ * prices = [1, 2, 3, 0, 2]
+ * maxProfit = 3
+ * transactions = [buy, sell, cooldown, buy, sell]
+ */
+int maxProfit(int* prices, int pricesSize);
+
 #endif
